0169e02b3b6ee5ed24a4042abb742b744833fbb7
AndrewElvisDeng <adeng1433@gmail.com>
2019-05-31 15:57:39 -0500

    associations updated

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,4 @@
 class Product < ApplicationRecord
-    belongs_to :stock, class_name: "stock", foreign_key: "stock_id"
     validates :name, presence: true
     validates :price, presence: true
 end
